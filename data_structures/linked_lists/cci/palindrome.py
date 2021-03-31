2417005fd2d0fcccd2ac1c7d9a2a87ce3b591d46
Carla d'Abreu <carlaamvieira@gmail.com>
2021-03-31 23:57:44 +0200

    ADD: cci ex

--- data_structures/linked_lists/cci/palindrome.py
+++ data_structures/linked_lists/cci/palindrome.py
@@ -1,1 +1,33 @@
-def palindrome(node):
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def is_palindrome(node):
+    pointer_fast = pointer_slow = node
+
+    stack = []
+
+    while pointer_fast and pointer_fast.next:
+        stack.append(pointer_slow.val)
+        pointer_slow = pointer_slow.next
+        pointer_fast = pointer_fast.next.next
+
+    if pointer_fast:
+        pointer_slow = pointer_slow.next
+
+    while pointer_slow:
+        top = stack.pop()
+        if top != pointer_slow.val:
+            return False
+        pointer_slow = pointer_slow.next
+
+    return True
+
+
+node1 = ListNode(1)
+node2 = ListNode(2, node1)
+node3 = ListNode(3, node2)
+node4 = ListNode(2, node3)
+node5 = ListNode(1, node4)
+print(is_palindrome(node5))
