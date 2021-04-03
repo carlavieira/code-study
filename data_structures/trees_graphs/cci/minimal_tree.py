5b3a7691d2a7d9f22ba404bafcb07f91595b1533
Carla d'Abreu <carlaamvieira@gmail.com>
2021-04-03 14:27:08 +0200

    ADD: cci ex.

--- data_structures/trees_graphs/cci/minimal_tree.py
+++ data_structures/trees_graphs/cci/minimal_tree.py
@@ -5,6 +5,8 @@ class Node:
 
 def minimal_tree(arr_int):
     def recursive_add(start, end):
+        if end < start:
+            return None
         if start == end:
             return Node(arr_int[start])
         mid = (start+end) // 2
@@ -14,3 +16,15 @@ def minimal_tree(arr_int):
     root = recursive_add(0, len(arr_int)-1)
 
     return root
+
+def print_tree(root):
+    if root:
+        print(f"[Node value: {root.val}, Children: ", end="")
+        for child in root.children:
+            if child:
+             print(child.val, end=" ")
+        print()
+        for child in root.children:
+            print_tree(child)
+
+print_tree(minimal_tree([1, 2, 3, 4, 5, 6]))
